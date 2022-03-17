d5deabd518c25b1ca55359b9ce108a22052101b3
Vaidas Sirtautas <v@vsshs.com>
2022-03-17 23:19:17 +0100

    Clear logging providers as per original logic

--- MovieServiceYouTube/Program.cs
+++ MovieServiceYouTube/Program.cs
@@ -1,9 +1,14 @@
+using System;
 using DomainLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MovieServiceCore3.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Clear logging providers as per original logic
+builder.Logging.ClearProviders();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -11,10 +16,11 @@ builder.Services.AddSingleton<DomainFacade>();
 
 var app = builder.Build();
 
+app.UseCustomExceptionHandling();
+
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-app.UseCustomExceptionHandling();
 
 app.MapRazorPages();
 app.MapControllers();
